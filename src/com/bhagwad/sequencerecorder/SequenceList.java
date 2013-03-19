e4d010d32f41fc6c28a2e0b312d27195470b5531
Bhagwad Jal Park <bhagwad@yahoo.com>
2013-03-19 11:46:16 -0400

    Create sequence details. Dummy listview to trigger

--- src/com/bhagwad/sequencerecorder/SequenceList.java
+++ src/com/bhagwad/sequencerecorder/SequenceList.java
@@ -1,8 +1,14 @@
 package com.bhagwad.sequencerecorder;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class SequenceList extends Activity {
 
@@ -10,6 +16,23 @@ public class SequenceList extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_sequence_list);
+		
+		ListView listViewSequence = (ListView) findViewById(R.id.listView_sequence_list);
+        
+        String[] items = new String[] {"Item 1", "Item 2", "Item 3"};
+        ArrayAdapter<String> adapter =
+          new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        
+        listViewSequence.setAdapter(adapter);
+        
+        listViewSequence.setOnItemClickListener(new OnItemClickListener() {
+
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				startActivity(new Intent(SequenceList.this, SequenceDetails.class));
+				
+			}
+		});
+
 	}
 
 	@Override
